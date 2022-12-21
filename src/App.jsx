0c4b6cb86dc806c8c864b2556d5f1d9ff2fdf3e3
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-12-21 19:31:19 +0000

    use state set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.scss";
 import Button from "./components/Button/Button";
 
 const App = () => {
+  const [char, setChar] = useState();
+
   const getUser = async () => {
     const response = await fetch("https://randomuser.me/api/");
     const data = await response.json();
-    console.log(data);
+    setChar(data.results[0]);
   };
 
   return (
