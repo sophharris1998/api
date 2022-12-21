8ef60480d541a1f9e7571b2944cd3164fb285156
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-12-21 18:41:57 +0000

    character profile console logging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.scss";
+import Button from "./components/Button/Button";
 
 const App = () => {
+  const getUser = async () => {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="app">
       <h1>Random User Generator</h1>
+      <Button label="Get Random User" onClick={getUser} />
     </div>
   );
 };
